4f7a54ee3d611f9ee815f96e92c936e66e988545
Esteban Centeno <luiscentenountiveros@gmail.com>
2023-03-08 17:32:51 -0500

    Version 2

--- Test_BNO55_MCP4725.py
+++ Test_BNO55_MCP4725.py
@@ -1,34 +1,32 @@
 import time
 import board
 import busio
-import adafruit_bno055
 import adafruit_mcp4725
+import adafruit_bno055
 
-# Inicializa el bus I2C
+# Configuramos el bus I2C
 i2c = busio.I2C(board.SCL, board.SDA)
 
-# Inicializa el BNO055
-bno = adafruit_bno055.BNO055(i2c)
-
-# Inicializa el MCP4725
+# Creamos un objeto MCP4725 sin argumentos
 dac = adafruit_mcp4725.MCP4725(i2c)
 
+# Creamos un objeto BNO055 sin argumentos
+sensor = adafruit_bno055.BNO055(i2c)
+
+# Configuramos la salida del DAC
+dac.normalized_value = 0.5
+
+# Leemos los valores de Euler del BNO055
 while True:
-    # Lee la orientación del BNO055
-    orientacion = bno.euler
+    heading, roll, pitch = sensor.euler
 
-    # Convierte la orientación a un valor de voltaje para el MCP4725
-    voltaje = orientacion[0] / 360 * 3.3
+    # Convertimos los valores a grados
+    heading_deg = heading % 360.0
+    roll_deg = roll
+    pitch_deg = pitch
 
-    # Convierte el voltaje a un valor de 12 bits para el MCP4725
-    valor = int(voltaje / 3.3 * 4095)
+    # Imprimimos los valores
+    print("Heading: {0:.2f} degrees, Roll: {1:.2f} degrees, Pitch: {2:.2f} degrees".format(heading_deg, roll_deg, pitch_deg))
 
-    # Escribe el valor en el MCP4725
-    dac.value = valor
-
-    # Imprime la orientación y el valor de voltaje en la consola
-    print("Orientación (heading, roll, pitch): {}".format(orientacion))
-    print("Valor de voltaje escrito en el MCP4725: {:.2f} V".format(valor / 4095 * 3.3))
-
-    # Espera un segundo antes de volver a leer la orientación del BNO055
-    time.sleep(1)
+    # Esperamos 1 segundo antes de leer los valores de nuevo
+    time.sleep(1.0)
