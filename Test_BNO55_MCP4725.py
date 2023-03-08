b8fbf73c7a5862d43a04af9f4eff112716381bf9
Esteban Centeno <luiscentenountiveros@gmail.com>
2023-03-08 18:28:29 -0500

    creo que esta es

--- Test_BNO55_MCP4725.py
+++ Test_BNO55_MCP4725.py
@@ -13,3 +13,22 @@ bno = adafruit_bno055.BNO055_I2C(i2c, address=0x28)
 # Inicializa el MCP4725 en la dirección 0x60
 dac = adafruit_mcp4725.MCP4725(i2c, address=0x60)
 
+while True:
+    # Lee la orientación del BNO055
+    orientacion = bno.euler
+
+    # Convierte la orientación a un valor de voltaje para el MCP4725
+    voltaje = orientacion[0] / 360 * 3.3
+
+    # Convierte el voltaje a un valor de 12 bits para el MCP4725
+    valor = int(voltaje / 3.3 * 4095)
+
+    # Escribe el valor en el MCP4725
+    dac.value = valor
+
+    # Imprime la orientación y el valor de voltaje en la consola
+    print("Orientación (heading, roll, pitch): {}".format(orientacion))
+    print("Valor de voltaje escrito en el MCP4725: {:.2f} V".format(valor / 4095 * 3.3))
+
+    # Espera un segundo antes de volver a leer la orientación del BNO055
+    time.sleep(1)
