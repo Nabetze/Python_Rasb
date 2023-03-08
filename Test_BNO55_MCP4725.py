23afba2eb5b99f8fc4e194f127901a2fa3fbec2d
Esteban Centeno <luiscentenountiveros@gmail.com>
2023-03-08 17:52:26 -0500

    a

--- Test_BNO55_MCP4725.py
+++ Test_BNO55_MCP4725.py
@@ -7,11 +7,8 @@ import adafruit_mcp4725
 # Inicializa el bus I2C
 i2c = busio.I2C(board.SCL, board.SDA)
 
-print(i2c[0])
-print(i2c[1])
-
 # Inicializa el BNO055
-bno = adafruit_bno055.BNO055(i2c)
+bno = adafruit_bno055.BNO055(i2c[0])
 
 # Inicializa el MCP4725
-dac = adafruit_mcp4725.MCP4725(i2c)
+dac = adafruit_mcp4725.MCP4725(i2c[1])
