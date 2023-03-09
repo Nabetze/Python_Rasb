1e861da34f33b1c8d371be010d5c8503c7b56bf1
Esteban Centeno <luiscentenountiveros@gmail.com>
2023-03-09 14:42:36 -0500

    con valor de presion inicial.

--- Test_BNO55_MCP4725.py
+++ Test_BNO55_MCP4725.py
@@ -13,22 +13,25 @@ bno = adafruit_bno055.BNO055_I2C(i2c, address=0x28)
 # Inicializa el MCP4725 en la dirección 0x60
 dac = adafruit_mcp4725.MCP4725(i2c, address=0x60)
 
+float limite = 30  #[kPA], máxima presion en el regulador electronico.
+float u = 0        #[kPa], valor inicial de la ley de control.
+
 while True:
     # Lee la orientación del BNO055
     orientacion = bno.euler
 
-    # Convierte la orientación a un valor de voltaje para el MCP4725
-    #voltaje = orientacion[0] / 360 * 3.3
+    # Valor de presión:
+
 
     # Convierte el voltaje a un valor de 12 bits para el MCP4725
-    valor = int(40 / 3.3 * 4095)
+    valor = int( u / limite * 4095)
 
     # Escribe el valor en el MCP4725
     dac.value = valor
 
     # Imprime la orientación y el valor de voltaje en la consola
     print("Orientación (heading, roll, pitch): {}".format(orientacion))
-    print("Valor de voltaje escrito en el MCP4725: {:.2f} V".format(valor / 4095 * 3.3))
+    print("Valor de voltaje escrito en el MCP4725: {:.2f} V".format(valor / 4095 * 5))
 
     # Espera un segundo antes de volver a leer la orientación del BNO055
-    time.sleep(1)
+    time.sleep(0.1)
