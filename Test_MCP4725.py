9bb688bd46c4425c2f3544dfa2eb1c59c090f57b
Esteban Centeno <luiscentenountiveros@gmail.com>
2023-03-13 17:24:35 -0500

    Con el voltaje de la Rasberry

--- Test_MCP4725.py
+++ Test_MCP4725.py
@@ -17,21 +17,25 @@ bno = adafruit_bno055.BNO055_I2C(i2c, address=0x28)
 # Inicializa el MCP4725 en la dirección 0x60
 dac = adafruit_mcp4725.MCP4725(i2c, address=0x60)
 
+# Voltaje máximo de entrada:
+Vol_rasp = 5.4  #Vol.
+
 while True:
     
     # Calcula el valor de salida del controlador PID
     u = float(input("Ingresar valor de voltaje: "))
 
+
     # Limita la salida a los límites del regulador electrónico (0-[limite])
     #u = min(max(u, 0), 5)
 
     # Convierte el voltaje a un valor de 12 bits para el MCP4725
-    valor = int( u / 5 * 65535)
+    valor = int( u / Vol_rasp * 65535)
 
     # Escribe el valor en el MCP4725
     dac.value = valor
 
-    print("El voltaje que se envió fue: {}".format(valor*5/65535))
+    print("El voltaje que se envió fue: {}".format(valor*Vol_rasp/65535))
 
     # Espera un segundo antes de volver a leer la orientación del BNO055
 
