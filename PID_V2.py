127c4fbb87e275089e8efa9c8608db1408db830b
Esteban Centeno <luiscentenountiveros@gmail.com>
2023-03-16 17:03:39 -0500

    a

--- PID_V2.py
+++ PID_V2.py
@@ -65,7 +65,7 @@ while True:
 
     # Enviar los datos a Node-RED
     #payload = {"angulo": orientacion[1], "output": u, "referencia": target}
-    payload = {"topic": "datos_sensor", "payload": orientacion[1]}
+    payload = {"topic": "medido", "payload": orientacion[1]}, {"topic": "referencia", "payload": target}
 
     client.publish(topic, json.dumps(payload))
 
