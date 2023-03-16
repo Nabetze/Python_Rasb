7458f4cffb870e491d5ce39c60ec0909253d43e6
Esteban Centeno <luiscentenountiveros@gmail.com>
2023-03-16 17:44:51 -0500

    a

---
+++ Test_Recepcion_NodeRed.py
@@ -0,0 +1,9 @@
+import sys
+
+# Leer los argumentos de línea de comando
+valor_numerico = int(sys.argv[1])
+valor_texto = sys.argv[2]
+
+# Hacer algo con los valores recibidos
+print("Valor numérico recibido:", valor_numerico)
+print("Valor de texto recibido:", valor_texto)
