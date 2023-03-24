eb57e86b12914b1fc39d706714b3583185160f41
Esteban Centeno <luiscentenountiveros@gmail.com>
2023-03-24 02:00:01 -0500

    Implementacion del Fuzzy

--- Test_Fuzzy_from_Matlab.py
+++ Test_Fuzzy_from_Matlab.py
@@ -1,11 +1,1 @@
 import skfuzzy as fuzz
-
-# Cargar el archivo .fis
-with open('PI_9.fis', 'r') as f:
-    fuzzy_control = fuzz.control.load_from_fis(f)
-
-# Acceder a los antecedentes y consecuentes del sistema de control difuso
-antecedents = fuzzy_control.antecedents
-consequents = fuzzy_control.consequents
-
-print(antecedents)
