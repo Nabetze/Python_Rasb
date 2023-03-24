596bfb39efa596e41e59bcb82b8fd73fc088ca86
Esteban Centeno <luiscentenountiveros@gmail.com>
2023-03-24 16:13:57 -0500

    a

--- FLC_V1.py
+++ FLC_V1.py
@@ -259,11 +259,8 @@ while True:
     simulacion.input['error'] = min(max(error_medido, 30), -30)
     simulacion.input['derror'] = min(max(derror_medido, 75), -75)
     
-    print(orientacion)
-    print(u)
-    print(error_medido)
-    print(derror_medido)
-    #print('Medido:{:2f} U:{:2f} Error:{:2f} Derror:{:2f}'.format(orientacion, u, error_medido, derror_medido))
+
+    print('Medido:{:2f} U:{:2f} Error:{:2f} Derror:{:2f}'.format(orientacion[1], u, error_medido, derror_medido))
 
     # Evalúa la salida del controlador difuso
     simulacion.compute()
