84f61effd79990e250dda9bfd0f93572732f82fa
Esteban Centeno <luiscentenountiveros@gmail.com>
2023-04-18 13:16:54 -0500

    a

---
+++ ESP32/Test_ESP32_V2.py
@@ -0,0 +1,18 @@
+import socket
+
+HOST = '192.168.43.101'
+PORT = 1234
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    print('Esperando conexión...')
+    conn, addr = s.accept()
+    with conn:
+        print('Conexión establecida por', addr)
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            num = int.from_bytes(data, byteorder='little')
+            print(num)
