61e024e2a5c1706b0c564abc48168cef336a8b76
Esteban Centeno <luiscentenountiveros@gmail.com>
2023-04-26 17:26:38 -0500

    a

--- Graficar_Tutorial/Tutorial_1.py
+++ Graficar_Tutorial/Tutorial_1.py
@@ -1,6 +1,6 @@
 import matplotlib
 matplotlib.use("TkAgg")
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg #, NavigationToolbar2TkAgg
 
 from matplotlib.figure import Figure
 import matplotlib.animation as animation
@@ -120,8 +120,8 @@ class BTCe_Page(tk.Frame):
         canvas.show()
         canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
 
-        toolbar = NavigationToolbar2TkAgg(canvas, self)
-        toolbar.update()
+        #toolbar = NavigationToolbar2TkAgg(canvas, self)
+        #toolbar.update()
         canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
 
