df28227421a8593278b34b293e728e14fdacaa76
Esteban Centeno <luiscentenountiveros@gmail.com>
2023-04-26 18:26:32 -0500

    a

--- Graficar_Tutorial/Tutorial_1.py
+++ Graficar_Tutorial/Tutorial_1.py
@@ -1,7 +1,6 @@
 import matplotlib
 matplotlib.use("TkAgg")
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
-
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 import matplotlib.animation as animation
 from matplotlib import style
