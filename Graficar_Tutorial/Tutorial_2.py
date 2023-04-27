00ec3d5c5905c18812ab087d6eafc46e83d30add
Esteban Centeno <luiscentenountiveros@gmail.com>
2023-04-26 20:23:53 -0500

    a

--- Graficar_Tutorial/Tutorial_2.py
+++ Graficar_Tutorial/Tutorial_2.py
@@ -125,12 +125,12 @@ def on_message(client, userdata, message):
 
 def animate(num, h, data):
 
-    h.set_data(range(len(data[1])), gData[1])
+    h.set_data(range(len(data[1])), data[1])
     
 
     #a.clear()
 
-    #a.plot(data[1])
+    a.plot(data[1])
 
     return h,
 
