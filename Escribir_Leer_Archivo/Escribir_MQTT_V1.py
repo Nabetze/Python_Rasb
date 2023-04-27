33f20bdc51c24f08ef96370741501056e7d542a7
Esteban Centeno <luiscentenountiveros@gmail.com>
2023-04-27 17:31:32 -0500

    a

--- Escribir_Leer_Archivo/Escribir_MQTT_V1.py
+++ Escribir_Leer_Archivo/Escribir_MQTT_V1.py
@@ -3,7 +3,7 @@
 # Librerias del MQTT:
 import paho.mqtt.client as mqtt
 import numpy as np
-import pandas as pd
+#import pandas as pd
 
 
 # Configuración del cliente MQTT
@@ -11,8 +11,8 @@ MQTT_BROKER = "10.100.236.192"
 MQTT_TOPIC = "datos/bno055"
 
 # Datos iniciales:
-global gData
-gData = np.array([])
+#global gData
+#gData = np.array([])
 
 # Función que se ejecuta cuando se recibe un mensaje MQTT
 def on_message(client, userdata, message):
@@ -21,15 +21,15 @@ def on_message(client, userdata, message):
 
     mensaje_decodificado = message.payload.decode("utf-8")
     angulo = float(mensaje_decodificado)
-    gData = np.append(gData, angulo)
+    #gData = np.append(gData, angulo)
 
-    if len(gData) > 20:
-        gData = np.delete(gData, 0)
+    #if len(gData) > 20:
+    #    gData = np.delete(gData, 0)
 
-    df = pd.DataFrame(data = gData)
+    #df = pd.DataFrame(data = gData)
 
     # Guardar el DataFrame en un archivo CSV
-    df.to_csv('Test.csv', index=False)
+    #df.to_csv('Test.csv', index=False)
 
     print(angulo)
 
